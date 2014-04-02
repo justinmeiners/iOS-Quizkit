f531343ecc20e74973f067b66bc7ac8125d6b9f6
Christian French <christian.french@kiddicare.com>
2014-04-02 10:10:44 +0100

    new base class for question view controllers

---
+++ Example/QuizKitSample/QuizKitSample/BaseQuestionViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseQuestionViewController.h
+//  QuizKitSample
+//
+//  Created by Christian French on 01/04/2014.
+//  Copyright (c) 2014 Infuse Medical. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseQuestionViewController : UIViewController
+
+@end
